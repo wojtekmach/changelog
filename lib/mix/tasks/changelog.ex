0acd3b353d6761cd239b46bed27a6e99631e20f9
Wojtek Mach <wojtek@wojtekmach.pl>
2018-01-27 21:27:41 +0100

    Handle latest versions

--- lib/mix/tasks/changelog.ex
+++ lib/mix/tasks/changelog.ex
@@ -10,42 +10,65 @@ defmodule Mix.Tasks.Changelog do
     case args do
       [name] ->
         changelog = fetch_changelog(name)
+        print_releases(changelog)
+
+      [name, "latest"] ->
+        changelog = fetch_changelog(name)
         release = List.first(changelog)
         print_release(release)
 
       [name, version] ->
         changelog = fetch_changelog(name)
-        release = Enum.find(changelog, &Version.compare(&1.version, version) == :eq)
+        release = Enum.find(changelog, &(Version.compare(&1.version, version) == :eq))
         print_release(release)
+
+      [name, version_from, "latest"] ->
+        changelog = fetch_changelog(name)
+
+        releases =
+          Enum.filter(changelog, &(Version.compare(&1.version, version_from) in [:eq, :gt]))
+
+        print_releases(releases)
 
       [name, version_from, version_to] ->
         changelog = fetch_changelog(name)
-        releases = Enum.filter(changelog, &Version.compare(&1.version, version_from) in [:eq, :gt] and Version.compare(&1.version, version_to) in [:eq, :lt])
-        Enum.each(releases, fn release ->
-          print_release(release)
-          IO.puts("")
-        end)
+        releases = Enum.filter(changelog, &match_version?(&1, version_from, version_to))
+        print_releases(releases)
 
       _ ->
-        Mix.raise """
+        Mix.raise("""
         Usage:
 
           mix changelog PACKAGE
+          mix changelog PACKAGE latest
           mix changelog PACKAGE VERSION
           mix changelog PACKAGE VERSION_FROM VERSION_TO
+          mix changelog PACKAGE VERSION_FROM latest
 
-        """
+        """)
     end
+  end
+
+  defp match_version?(release, version_from, version_to) do
+    Version.compare(release.version, version_from) in [:eq, :gt] and
+      Version.compare(release.version, version_to) in [:eq, :lt]
+  end
+
+  defp print_releases(releases) do
+    Enum.each(releases, fn release ->
+      print_release(release)
+      IO.puts("")
+    end)
   end
 
   defp print_release(release) do
     if release.date do
-      IO.puts "## #{release.version} - #{release.date}"
+      IO.puts("## #{release.version} - #{release.date}")
     else
-      IO.puts "## #{release.version}"
+      IO.puts("## #{release.version}")
     end
 
-    IO.puts ""
+    IO.puts("")
     Enum.each(release.notes, &IO.puts(&1))
   end
 
@@ -82,11 +105,11 @@ defmodule Mix.Tasks.Changelog do
     {:ok, vsn} = :application.get_key(:hex, :vsn)
 
     unless Version.match?("#{vsn}", "~> 0.17.1") do
-      Mix.raise """
+      Mix.raise("""
       changelog requires Hex ~> 0.17.1, got: #{vsn}. Please upgrade!
 
       Also, stay tuned for a new changelog release that is not naughy and does not use Hex private APIs anymore!
-      """
+      """)
     end
   end
 end
