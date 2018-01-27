0fa5ab804e663984d851b196b3a7ada05607b55b
Wojtek Mach <wojtek@wojtekmach.pl>
2018-01-27 21:44:14 +0100

    Add CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Changelog.MixProject do
   def project do
     [
       app: :changelog,
-      version: "0.1.0",
+      version: "0.1.0-dev",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
